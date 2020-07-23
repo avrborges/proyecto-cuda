0e28a3395ae9f239fa67fb41ac7b7594d268c868
Alejandro Borges <avrborges@gmail.com>
2020-07-23 17:49:48 -0300

    Se configura el form de registro

--- src/app/auth/registro/registro.component.ts
+++ src/app/auth/registro/registro.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Validators, FormBuilder, FormGroup, FormControl} from '@angular/forms';
+
 
 @Component({
   selector: 'app-registro',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroComponent implements OnInit {
 
-  constructor() { }
+  formRegistro: FormGroup;
+
+  constructor(
+    private _fromBuilder: FormBuilder,
+  ) { 
+    this.formRegistro = this._fromBuilder.group({
+      email:['',[Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$")]],
+      password:['', [Validators.required, Validators.minLength(8)]]
+    })
+  }
 
   ngOnInit(): void {
   }
 
+  onSubmit(){
+    console.log("Registro -> ", this.formRegistro.value);
+  }
 }
